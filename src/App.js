d1d589aad009697c13fa2c55d7aa3954a1e07807
yumauma20 <71yuma10@gmail.com>
2020-09-20 10:42:37 +0900

    view 作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const data = [
+  {
+    id: 1,
+    name: "リオネル・メッシ",
+    team: "FCバルセロナ",
+    description:
+      "アルゼンチンサンタフェ州ロサリオ出身のイタリア系アルゼンチン人サッカー選手。リーガ・エスパニョーラ・FCバルセロナ所属。アルゼンチン代表。ポジションはフォワード (wikipedia)",
+    isFollow: false,
+  },
+  {
+    id: 2,
+    name: "クリスティアーノ・ロナウド",
+    team: "Juventus",
+    description:
+      "ポルトガル・フンシャル出身のサッカー選手。セリエA・ユヴェントスFC所属。ポルトガル代表。ポジションはフォワード (wikipedia)",
+    isFollow: true,
+  },
+  {
+    id: 3,
+    name: "ネイマール",
+    team: "パリサンジェルマン",
+    description:
+      "ブラジル・サンパウロ州モジ・ダス・クルーゼス出身のサッカー選手。ブラジル代表。リーグ・アン・パリ・サンジェルマンFC所属。ポジションはフォワード (wikipedia)",
+    isFollow: false,
+  },
+];
 
 function App() {
+  const [accounts, setAccounts] = React.useState(data);
+  const onClickFollow = React.useCallback(
+    (id) => {
+      const updatedAccounts = accounts.map((e) => {
+        if (e.id === id) {
+          return {
+            ...e,
+            isFollow: !e.isFollow,
+          };
+        } else {
+          return e;
+        }
+      });
+      setAccounts(updatedAccounts);
+    },
+    [accounts, setAccounts]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul class="accountList">
+      {accounts.map((e) => {
+        return (
+          <li key={e.id} class="accountList__item">
+            <div>
+              <div class="account__summary">
+                <div>
+                  <p class="account__name">{e.name}</p>
+                  <p class="account__team">{e.team}</p>
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    class={`followBtn ${e.isFollow ? "isFollow" : ""}`}
+                    onClick={() => onClickFollow(e.id)}
+                  >
+                    {e.isFollow ? "フォロー中" : "フォローする"}
+                  </button>
+                </div>
+              </div>
+              <p class="account__description">{e.description}</p>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
